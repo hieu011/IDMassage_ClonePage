1a49aaf4607ba5c5a34cca85621e471ee11878d2
Ngo Manh Thong <ngomanhthong0812>
2025-03-03 10:49:03 +0700

    update page product

--- app/[lang]/product/page.tsx
+++ app/[lang]/product/page.tsx
@@ -14,14 +14,14 @@ const ProductDetailPage = async ({ params }: {
         <div className="bg-[#f5f8fd]">
             <div className="bg-[#f9f9f9] py-2 overflow-auto">
                 <div className="max-w-6xl px-2 m-auto flex items-center gap-2 text-[13px]">
-                    <Link href={'/'} className="hover:text-[var(--header-color-)] text-nowrap">Trang chủ</Link>
+                    <Link href={`/${lang}`} className="hover:text-[var(--header-color-)] text-nowrap">Trang chủ</Link>
                     <span>/</span>
-                    <Link href={'/'} className="hover:text-[var(--header-color-)] text-nowrap">P. 1 - Q. 2 - TP. NHA TRANG</Link>
+                    <Link href={`/${lang}`} className="hover:text-[var(--header-color-)] text-nowrap">P. 1 - Q. 2 - TP. NHA TRANG</Link>
                     <span>/</span>
                     <p className="uppercase text-[var(--header-color-)] text-nowrap">{productDetailText.service_type}</p>
                 </div>
             </div>
-            
+
             <div className="py-3 px-2">
                 <ProductDetails productDetailText={productDetailText} />
                 <Services productDetailText={productDetailText} locale={lang} />
